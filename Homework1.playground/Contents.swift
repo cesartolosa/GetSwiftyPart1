52e22bed3e53a1c37fd293498614bec04fc7d6a4
Cesar Tolosa Franco <cesar.tolosa.franco@gmail.com>
2019-07-03 14:49:54 -0500

    chore: minor changes to Homework 1

--- Homework1.playground/Contents.swift
+++ Homework1.playground/Contents.swift
@@ -1,29 +1,35 @@
 /*
+ 
  Homework Assignment #1: Variables and Constants
  My favorite song
+ 
  */
 
+// Constants declaration
+
 let title: String = "Got It in You (Acoustic)"
-let artist: String = "banner".uppercased()
+let artist: String = "banner".uppercased() // Use of string function
 let genre: String = "Alterenative"
-let duration: (Int8, Int8) = (3, 40)
-let album: String = title + " - Single"
-let released: (Int8, String, Int16) = (28, "february", 2019)
+let (minutes, seconds): (Int8, Int8) = (3, 40)
+let duration: String = "\(minutes):\(seconds)" // Use of string interpolation
+let album: String = title + " - Single" // String concatenation
+let (day, month, year): (Int8, String, Int) = (28, "february", 2019)
 let compositor: String = "Sarah Emily Berrios & Michael Nelson"
 let copyright: String = "℗ 2019 Island Records, a division of UMG Recordings, Inc."
-var liked: Bool = true
 
-let (minutes, seconds) = duration
-let (day, month, year) = released
+// Variables declaration
+
+var liked: Bool = true
 var likedIndicator: Character = liked ? "❤️" : "💔";
+
+// Print variables into console
 
 print(title)
 print(artist)
 print(genre)
-print("\(minutes):\(seconds)")
+print(duration)
 print(album)
 print(day, month, year)
 print(compositor)
 print(copyright)
 print(likedIndicator)
-
